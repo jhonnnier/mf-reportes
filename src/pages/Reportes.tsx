27acf101cbd02aee16af51ebd680cf9ea2a1181a
Andrés Torres <ctorres437@hotmail.com>
2025-08-24 20:05:24 -0500

    feat: añadido fetch

--- src/pages/Reportes.tsx
+++ src/pages/Reportes.tsx
@@ -1,7 +1,23 @@
+
+import { useEffect, useState } from "react";
+
 export default function Reportes(props) {
+  const [reportes, setReportes] = useState("NA")
+
+  useEffect(() => {
+    fetch("http://localhost:5003/reportes")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Respuesta de /reportes:", data.reportes);
+        setReportes(data.reportes)
+      })
+      .catch((error) => {
+        console.error("Error en la petición /estudiantes:", error);
+      });
+  }, []);
   return (
-      <div>
-          <h3>Reportes</h3>
-      </div>
+    <div>
+      <h3>{reportes}</h3>
+    </div>
   );
 }
